25a251316a1d0574c2723dc72d9397ea5d982eb5
txjiang <jiangtaoxin1993@gmail.com>
2017-05-19 16:40:04 -0400

    DeepLearning_TUT

---
+++ Tutorial_basic_stuff.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+'''
+Normalize Input:
+Always have variables to have zero-mean and equal variance (normalization)
+
+Random Initialization (initial weight and bias):
+random_normal (use zero_mean, small std)
+
+'''
